86cb8860c7a6cb82cf91298cfabb8812eb7a0262
HUAJIAN <1415885403@qq.com>
2021-02-04 20:08:21 +0800

    add Cutoff

--- Assets/CustomRenderPipeline/ShaderLibrary/UnlitPass.hlsl
+++ Assets/CustomRenderPipeline/ShaderLibrary/UnlitPass.hlsl
@@ -3,17 +3,24 @@
 
 #include "../ShaderLibrary/Common.hlsl"
 
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap);
+
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
     UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+    UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct VertexInput {
     float4 pos : POSITION;
+    float2 baseUV : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct VertexOutput {
     float4 clipPos : SV_POSITION;
+    float2 baseUV : VAR_BASE_UV;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -24,13 +31,22 @@ VertexOutput UnlitPassVertex(VertexInput input)
     UNITY_TRANSFER_INSTANCE_ID(input, output);
     float3 worldPos = TransformObjectToWorld(input.pos.xyz);
     output.clipPos = TransformWorldToHClip(worldPos);
+    float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.baseUV = input.baseUV * baseST.xy +  baseST.zw; 
     return output;                   
 }
 
 float4 UnlitPassFragment(VertexOutput input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
+    float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 color = baseMap * baseColor;
+    #if defined(_CLIPPING)
+        clip(color.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
+    #endif
+
+    return color;    
 }
 
-#endif // CUSTOM_UNLIT_PASS_INCLUDED
+#endif // CUSTOM_UNLIT_PASS_INCLUDED                                    
