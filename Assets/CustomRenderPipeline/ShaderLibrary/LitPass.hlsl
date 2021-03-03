5f71b65f98887470ca13ef8eef984c4fff9ba837
HUAJIAN <1415885403@qq.com>
2021-03-03 21:06:54 +0800

    shadow

--- Assets/CustomRenderPipeline/ShaderLibrary/LitPass.hlsl
+++ Assets/CustomRenderPipeline/ShaderLibrary/LitPass.hlsl
@@ -22,15 +22,15 @@ UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct VertexInput {
-    float3 pos : POSITION;
+    float3 poistion : POSITION;
     float2 baseUV : TEXCOORD0;
     float3 normal : NORMAL;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct VertexOutput {
-    float4 clipPos : SV_POSITION;
-    float3 posWS : VAR_POSITION;    
+    float4 positionCS : SV_POSITION;
+    float3 positionWS : VAR_POSITION;    
     float2 baseUV : TEXCOORD1;
     float3 normal : TEXCOORD2;
     UNITY_VERTEX_INPUT_INSTANCE_ID
@@ -42,12 +42,19 @@ VertexOutput LitPassVertex(VertexInput input)
     VertexOutput output;
     UNITY_SETUP_INSTANCE_ID(input);
     UNITY_TRANSFER_INSTANCE_ID(input, output);
-    float3 worldPos = TransformObjectToWorld(input.pos);
-    output.clipPos = TransformWorldToHClip(worldPos);
+    float3 worldPos = TransformObjectToWorld(input.poistion);
+    output.positionCS = TransformWorldToHClip(worldPos);
+    #if UNITY_REVERSED_Z
+        output.positionCS.z = min(output.positionCS.z, output.positionCS.w * UNITY_NEAR_CLIP_VALUE);
+    #else
+        output.positionCS.z =
+                    max(output.positionCS.z, output.positionCS.w * UNITY_NEAR_CLIP_VALUE);                                   
+    #endif
+    
     float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
     output.baseUV = input.baseUV * baseST.xy +  baseST.zw; 
     output.normal = TransformObjectToWorldNormal(input.normal);
-    output.posWS = TransformObjectToWorld(input.pos.xyz); 
+    output.positionWS = TransformObjectToWorld(input.poistion.xyz); 
     return output;                   
 }
 
@@ -61,14 +68,14 @@ float4 LitPassFragment(VertexOutput input) : SV_TARGET
     clip(base.a - UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff));
     #endif                                   
     Surface surface;
-    surface.position = input.posWS;
+    surface.position = input.positionWS;
     surface.normal = normalize(input.normal);
     surface.color = base.rgb;
     surface.alpha = base.a;
     surface.metallic = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
     surface.smoothness = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Smoothness);
-    surface.viewDirection = normalize(_WorldSpaceCameraPos - input.posWS);
-    surface.depth = -TransformWorldToView(input.posWS).z;
+    surface.viewDirection = normalize(_WorldSpaceCameraPos - input.positionWS);
+    surface.depth = -TransformWorldToView(input.positionWS).z;
     #if defined(_PREMULTIPLY_ALPHA)
         BRDF brdf = GetBRDF(surface, true);
     #else
